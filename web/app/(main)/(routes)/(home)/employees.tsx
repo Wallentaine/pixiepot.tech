9e64573c48bea8eede133f3f64cca23b6a4652ef
Wallentaine <wallentaine@gmail.com>
2023-11-26 11:45:19 +0300

    Revert "fix hosts"
    This reverts commit 0e9e5a102671e9dfdb784f21a13897c2a26ba648.

--- web/app/(main)/(routes)/(home)/employees.tsx
+++ web/app/(main)/(routes)/(home)/employees.tsx
@@ -13,7 +13,7 @@ const Employees = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "http://172.26.0.1:4001/api/statistic/employees-project"
+          "http://localhost:4001/api/statistic/employees-project"
         );
         setEmployees(response.data);
       } catch (error) {
