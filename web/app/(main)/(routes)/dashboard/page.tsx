0e9e5a102671e9dfdb784f21a13897c2a26ba648
Wallentaine <wallentaine@gmail.com>
2023-11-26 11:42:49 +0300

    fix hosts

--- web/app/(main)/(routes)/dashboard/page.tsx
+++ web/app/(main)/(routes)/dashboard/page.tsx
@@ -39,7 +39,7 @@ const Dashboard = () => {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     const response = axios.post(
-      "http://localhost:4001/api/statistic/weight-data",
+      "http://172.26.0.1:4001/api/statistic/weight-data",
       data
     );
   };
